6d44898d9ad8998543c83761b25e8923e105967c
Dyrehauge <dthomasn@gmail.com>
2015-04-12 20:17:52 +0200

    setup

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>AppleFlat</title>
+	<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,700,100' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="center">
+	<div class="iphone-wrap">
+		<div class="iphone-structure">
+			<div class="camera"></div>
+			<div class="centerit">
+				<div class="dot"></div>
+				<div class="speaker"></div>
+			</div>
+			<div class="glare"></div>
+			<div class="screen">
+				<!-- content goes here  -->
+			</div>
+			<div class="home"></div>
+			
+		</div>
+	</div>
+
+	<div class="ipad-wrap">
+		<div class="ipad-structure">
+			<div class="glare"></div>
+			<div class="camera"></div>
+			<div class="screen">
+				<!-- content goes here  -->
+			</div>
+			<div class="home"></div>
+			
+		</div>
+	</div>
+
+	<div class="macbook-wrap">
+		<div class="macbook-structure">
+			<div class="glare"></div>
+			<div class="camera"></div>
+			<div class="screen">
+				<!-- content goes here  -->
+			</div>
+		</div>
+			<div class="keyboard"><div class="openarea"></div></div>
+			<div class="box"></div>
+	</div>
+
+	<div class="imac-wrap">
+		<div class="imac-structure">
+			<div class="glare"></div>
+			<div class="camera"></div>
+			<div class="screen">
+				<!-- content goes here  -->
+			</div>
+		</div>
+			<div class="whitespace"></div>
+			<div class="stand"></div>
+			<div class="footer"></div>
+	</div>
+</div>
+</body>
+</html>
